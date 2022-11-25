10926a5923d91522b9db6a31325fce7873cc1bbf
Mike Stefan Kock <mkock3@wbsedu.de>
2022-11-25 12:24:42 +0100

    added database schema

---
+++ controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../database/database_connection.php';
+
+$db = connectToDatabase();
+
+function createUser($name, $email, $password, $avatar) {
+
+}
+
+function deleteUser($id){
+
+}
